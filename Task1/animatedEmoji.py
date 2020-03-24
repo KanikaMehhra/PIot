94b362f82f4d032bdce5a7f366d356d9f4e37ede
Kanika Mehra <s3688154@student.rmit.edu.au>
2020-03-24 17:30:46 +1100

    created sad emoji

--- Task1/animatedEmoji.py
+++ Task1/animatedEmoji.py
@@ -1,6 +1,7 @@
 
 from MyClasses.Color import Color
 from MyClasses.Emoji import Emoji
+from time import sleep
 
 color=Color()
 
@@ -20,6 +21,20 @@ happyEmoji=Emoji([
     ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
 ], red, green, blue)
 
+sadEmoji=Emoji([
+    ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
+    ['b', 'o', 'o', 'f', 'f', 'o', 'o', 'b'],
+    ['b', 'o', 'o', 'f', 'f', 'o', 'o', 'b'],
+    ['b', 'f', 'f', 'f', 'f', 'f', 'f', 'b'],
+    ['b', 'f', 'o', 'o', 'o', 'o', 'f', 'b'],
+    ['b', 'o', 'o', 'o', 'o', 'o', 'o', 'b'],
+    ['b', 'o', 'f', 'f', 'f', 'f', 'o', 'b'],
+    ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
+],red,green,blue)
+
 
 while True:
     happyEmoji.drawEmoji()
+    sleep(2)
+    sadEmoji.drawEmoji()
+
